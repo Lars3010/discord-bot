b81b727ce61f245d15e0b24ab5e9660e92336bf1
Lars Roovers <larsroovers@gmail.com>
2019-11-30 02:17:08 +0100

    added test schedule with message to set channel

--- bot.js
+++ bot.js
@@ -1,20 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const {prefix, token} = require('./config.json');
+const schedule = require('node-schedule');
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.on('ready', () => {
+client.once('ready', () => {
     console.log(`logged in as ${client.user.tag}`);
+    const announcementChannel = client.channels.find(channel => channel.name === 'announcement-channel');
+
+    let job = schedule.scheduleJob('*/10 * * * * *', () => {
+        console.log('tick');
+        announcementChannel.send(`Time: ${Date()}`);
+    })
 });
 
 //ping pong
-client.on('message', msg => {
-    if(msg.content === `${prefix}ping`)
-    {
-        msg.channel.send('pong');
-    }
-})
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const arguments = message.content.slice(prefix.length).split(/ +/);
+    const command = arguments.shift().toLowerCase();
+});
 
 // login to Discord with your app's token
 client.login(token);
