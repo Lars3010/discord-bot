bf416abc45f15cc1ccaf42f506baabf815d049ab
Lars Roovers <larsroovers@gmail.com>
2019-12-17 01:52:01 +0100

    added emojis to announcement message

--- bot.js
+++ bot.js
@@ -3,15 +3,27 @@ const client = new Discord.Client();
 const {prefix, token} = require('./config.json');
 const schedule = require('node-schedule');
 
+function getEmoji(emojiName, backupName)
+{
+    let selectedEmoji = client.emojis.find(emoji => emoji.name === emojiName)
+    selectedEmoji = (selectedEmoji === null) ? backupName : selectedEmoji;
+    return selectedEmoji;
+}
+
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
     console.log(`logged in as ${client.user.tag}`);
-    const announcementChannel = client.channels.find(channel => channel.name === 'announcement-channel');
+    const announcementChannel = client.channels.find(channel => channel.name === 'dev-channel');
+    const agree_emoji = getEmoji('Agree', ':white_check_mark:');
+    const disagree_emoji = getEmoji('Disagree', ':no_entry:');
+
+    let dt = new Date();
+    let day = dt.getDay()
+    
 
     let job = schedule.scheduleJob('*/10 * * * * *', () => {
-        console.log('tick');
-        announcementChannel.send(`Time: ${Date()}`);
+        announcementChannel.send(`please ${agree_emoji} or ${disagree_emoji} if you will be able to make it to the next OP on Saturday the 23rd. please change your emoji depending on your availability.`);
     })
 });
 
