32f0ffd90f46cb8931062bb8cb0b30fd92dd1904
Lars Roovers <larsroovers@gmail.com>
2019-12-20 03:12:17 +0100

    WIP getting dates to include in eventmessage.

--- bot.js
+++ bot.js
@@ -17,13 +17,46 @@ client.once('ready', () => {
     const announcementChannel = client.channels.find(channel => channel.name === 'dev-channel');
     const agree_emoji = getEmoji('Agree', ':white_check_mark:');
     const disagree_emoji = getEmoji('Disagree', ':no_entry:');
+    let eventDates = new Array();
 
+    //this works but it can be done cleaner.
     let dt = new Date();
-    let day = dt.getDay()
-    
+    console.log(dt);
+    let currentDay = dt.getDay();
+    console.log(currentDay);
+    let diff = 6 - currentDay;
+    console.log(diff);
+    let saturdayDate = new Date(dt);
+    saturdayDate.setDate(dt.getDate() + diff);
+    saturdayDate.setUTCHours(18);
+    saturdayDate.setUTCMinutes(00);
+    saturdayDate.setUTCSeconds(00);
+    let sundayDate = new Date(saturdayDate);
+    sundayDate.setDate(dt.getDate() + diff + 1);
+    console.log(sundayDate);
+
+    eventDates.push(saturdayDate, sundayDate);
+    console.log(eventDates);
+
+    function getDates(dt)
+    {
+        dt = new Date(dt);
+        console.log(dt);
+         
+    }
+
+    const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
 
     let job = schedule.scheduleJob('*/10 * * * * *', () => {
-        announcementChannel.send(`please ${agree_emoji} or ${disagree_emoji} if you will be able to make it to the next OP on Saturday the 23rd. please change your emoji depending on your availability.`);
+        //announcementChannel.send(`please ${agree_emoji} or ${disagree_emoji} if you will be able to make it to the next OP on Saturday the 23rd. please change your emoji depending on your availability.`);
     })
 });
 
